7cebae9047db3c75fc947d9627faabce62c44cf3
elliottcable <git@elliottcable.name>
2008-10-02 17:03:07 -0800

    Make specs command-safe

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
 ($:.unshift File.expand_path(File.join( File.dirname(__FILE__), '..', 'lib' ))).uniq!
 require 'refinery'
+
+# Require spec here, for things like autotest and rdebug that aren't running
+# using the Rakefile.
+require 'spec'
