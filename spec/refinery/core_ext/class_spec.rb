6cbc5d29e248f70c158cd8a18b3859e389aaf149
elliottcable <git@elliottcable.name>
2008-10-16 03:50:47 -0800

    Cleaned up the spec for Class#instances

--- spec/refinery/core_ext/class_spec.rb
+++ spec/refinery/core_ext/class_spec.rb
@@ -2,21 +2,21 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Class do
   before :each do
-    @klass = Class.new
+    @class = Class.new
   end
   
   describe '#eigen?' do
     
     it 'should return false for a normal class' do
-      @klass.should_not be_eigen
+      @class.should_not be_eigen
     end
     
     it 'should return true for an eigenclass' do
-      @klass.eigenclass.should be_eigen
+      @class.eigenclass.should be_eigen
     end
     
     it 'should return true for a metaeigenclass' do
-      @klass.eigenclass.eigenclass.should be_eigen
+      @class.eigenclass.eigenclass.should be_eigen
     end
     
   end
@@ -24,10 +24,8 @@ describe Class do
   describe '#instances' do
     
     it 'should return all instances of a class currently present in the Ruby ObjectSpace' do
-      instance1 = @klass.new
-      instance2 = @klass.new
-      instance3 = @klass.new
-      [instance1, instance2, instance3].each {|instance| @klass.instances.should include(instance) } 
+      examples = [@class.new, @class.new, @class.new]
+      examples.each {|instance| @class.instances.should include(instance) } 
     end
     
   end
